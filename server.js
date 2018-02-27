c13e4f4b9ae8b28f22f42bda89820be254d40c0f
chris <chrisrogers1017@gmail.com>
2018-02-26 19:38:40 -0700

    server stuff

--- server.js
+++ server.js
@@ -4,8 +4,33 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 6969;
 
+var reservations = [
+  {
+    Name: "yoda",
+    phone: "303030303",
+    email: "Jedi Master",
+    unique: 900,
+  }
+];
+
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/table", function(req, res) {
+  res.sendFile(path.join(__dirname, "table.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/api/:characters", function(req, res) {
+  var chosen = req.params.characters;
+
+  if (chosen === "tables" || chosen === "waitlist") {
+    console.log(chosen);
+  }
 });
 
 app.listen(PORT, function() {
